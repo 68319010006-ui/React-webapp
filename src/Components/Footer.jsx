37a996e6287b8f115445a684397eca084e32aa85
68319010006-ui <68319010006@skntc.ac.th>
2025-08-20 18:07:19 +0700

    20/08/68

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+         <footer>
+ &#169; {`${new Date().getFullYear()} - React Developer`}
+ </footer>
+    </div>
+  )
+}
+
+export default Footer
