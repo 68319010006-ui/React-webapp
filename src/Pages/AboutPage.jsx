729ccc3d79175e4a09dada239ae4dd09f2d4e553
68319010006-ui <68319010006@skntc.ac.th>
2025-08-21 17:51:11 +0700

    21/08/68

---
+++ src/Pages/AboutPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <>
+    <div className = "p-6 bg-white rouded-md shadow-sm">
+      <h1 className="font bold text-2xl mb-3">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi dolor
+        illum perferendis, libero debitis dolorem nostrum quasi quos dicta earum
+        quisquam nesciunt accusantium dolores aliquam a, cum est et ratione
+        molestiae repellat consequuntur minus. Placeat, dolore doloribus vel
+        distinctio sunt ex at officiis. Vero reprehenderit quo accusantium
+        temporibus nulla mollitia!
+      </p>
+      </div>
+    </>
+  );
+}
+
+export default AboutPage;
