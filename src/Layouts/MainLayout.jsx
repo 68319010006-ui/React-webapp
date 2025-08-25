90035e4919e42030a40612ef3df51e6f20677d1d
68319010006-ui <68319010006@skntc.ac.th>
2025-08-25 16:49:27 +0700

    25/8/68 16.49

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,20 +1,20 @@
-
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
-import React from 'react'
+import React from "react";
 import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
     <>
-         <Navbar/>
-         <main>
-            <div className = "px-4 md:px-6 pt-12 pb-8 w-full xl :w-[45%] space-y-6"></div>
-            <Outlet/>
-         </main>
-         <Footer/>
+      <Navbar />
+      <main>
+        <div className="px-4 md:px-6 pt-12 pb-8 w-full xl:w-[59%] space-y-6">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Newtask from '../Components/Newtask'
+import TodoItem from '../Components/TodoItem';
 function HomePage() {
+  const [todos, setTodos] = useState([]);
+    const addTask = (task) =>{
+      setTodos((prevTodos) => [...prevTodos,task])
+  };
   return (
-    <div>HomePage</div>
+    <>
+    <Newtask addTask={addTask}/>
+    <ul className="bg-gray-200 rounded-md shadow-sm p-4">
+      {
+        todos.map((todo, i)=> (
+          <TodoItem key={i} id={i} todo={todo}/>
+        ))
+      }
+      
+    </ul>
+    </>
   )
 }
 
